46b74cce5d62e1a9ea0792d5b03f5d734195b78a
Rody Huang <rh624@ic.ac.uk>
2024-12-05 23:18:27 +0000

    overall stylish adjustments

--- src/app.py
+++ src/app.py
@@ -31,7 +31,7 @@ def register():
     return render_template("register_page.html")
 
 
-#if __name__ == "__main__":
-#app.run(debug=True)
-#port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
-#app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+# app.run(debug=True)
+# port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+# app.run(host="0.0.0.0", port=port)
