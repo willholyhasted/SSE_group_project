41203c0e1aab712084595bb9015f0a8d699636bb
cc2224 <chieh-tung.cheng24@imperial.ac.uk>
2024-12-06 18:14:27 +0000

    feat:add_test

--- src/app.py
+++ src/app.py
@@ -24,10 +24,9 @@ app.secret_key = "your_secret_key"
 
 # Function to clean up database connections
 @app.teardown_appcontext
-def cleanup(expection=None):
+def cleanup(exception=None):
     # Call the close_db function to close the database connection
-    close_db(expection)
-
+    close_db(exception)
 
 # Define a route for the home page ("/")
 @app.route("/")
@@ -36,7 +35,3 @@ def index():
     return render_template("index.html")
 
 
-# if __name__ == "__main__":
-# app.run(debug=True)
-# port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
-# app.run(host="0.0.0.0", port=port)
---
+++ src/test/test_config.py
@@ -0,0 +1,8 @@
+# Enable testing mode in Flask
+TESTING = True
+
+# Enable debug mode for more detailed error messages during testing
+DEBUG = True
+
+# Set a secret key for session handling
+## SECRET_KEY = "test_secret_key"
